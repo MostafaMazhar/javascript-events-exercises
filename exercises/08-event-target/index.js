335d95f8c2876d7caf41032bfb506a092cb6099d
Mostafa Mazhar <moustafammazhar@gmail.com>
2023-06-07 15:04:58 -0400

    added event.target.tagName

--- exercises/08-event-target/index.js
+++ exercises/08-event-target/index.js
@@ -2,5 +2,8 @@ window.onload = function loadFn() {
 	var containerElm = document.getElementById("container");
 	containerElm.addEventListener("click", function(event) {
 		//you code here
+    let tagNameTarget = event.target.tagName;
+     alert(`Clicked element tag name: ${tagNameTarget}`);
+
 	});
 };
