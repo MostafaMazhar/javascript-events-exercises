468bf060b74add34dc81f80becdabfe391b0f86b
Mostafa Mazhar <moustafammazhar@gmail.com>
2023-06-07 12:47:48 -0400

    06 add event listener js

--- exercises/06-add-listener-with-js/index.js
+++ exercises/06-add-listener-with-js/index.js
@@ -1,6 +1,11 @@
 window.onload = function myLoadFunction() {
 	alert("The website just finished loading!");
 	//some code here
+	let greenButton = document.getElementById("theGreen");
+	greenButton.addEventListener('click', clickHandler);
 };
 
 //the listener function here
+function clickHandler(){
+	alert("Wohooo");
+}
