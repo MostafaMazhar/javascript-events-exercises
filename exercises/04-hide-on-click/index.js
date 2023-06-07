82c0a1eeb01c47163f174b4a6040de4c9552b446
Mostafa Mazhar <moustafammazhar@gmail.com>
2023-06-07 12:16:32 -0400

    hide onclick

--- exercises/04-hide-on-click/index.js
+++ exercises/04-hide-on-click/index.js
@@ -1,4 +1,5 @@
-window.myEventListener = function myEventListener()
-{
-	//your code here
+window.myEventListener = function myEventListener(){
+
+document.getElementById('firstDiv').style.display = 'none';
+
 }
